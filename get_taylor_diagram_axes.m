84d21c31fa6af86ebca0fa865066813d942d7ada
Peter Rochford <prochford@thesymplectic.com>
2019-01-04 21:36:22 -0500

    Suppressed x-limits when overlay.

--- get_taylor_diagram_axes.m
+++ get_taylor_diagram_axes.m
@@ -37,17 +37,28 @@ if ~isfield(option, 'axismax')
 else
 	maxrho = option.axismax;
 end
-hhh = line([-maxrho -maxrho maxrho maxrho],[-maxrho maxrho maxrho -maxrho],'parent',cax);
-set(cax,'dataaspectratio',[1 1 1],'plotboxaspectratiomode','auto');
-v = [get(cax,'xlim') get(cax,'ylim')];
-ticks = sum(get(cax,'ytick')>=0);
-delete(hhh);
+
+% Determine default number of tick marks
+if strcmp(option.overlay,'off')
+    xlim([-maxrho maxrho]);
+    xt = xticks;
+    dx = xt(2) - xt(1);
+    if xt(1) > -maxrho
+        xt = [xt(1) - dx; xt(:)];
+    end
+    if xt(end) < maxrho
+        xt = [xt(:); xt(end) + dx];
+    end
+else
+    xt = xticks;
+end
+ticks = sum(xt >= 0);
 
 % check radial limits and ticks
 axes.rmin = 0; 
 if ~isfield(option, 'axismax')
-	axes.rmax = v(4);
-  option.axismax = axes.rmax;
+	axes.rmax = xt(end);
+    option.axismax = axes.rmax;
 else
 	axes.rmax = option.axismax;
 end
@@ -59,7 +70,7 @@ if rticks > 5   % see if we can reduce the number
         rticks = rticks/3;
     end
 end
-axes.rinc  = (axes.rmax - axes.rmin)/rticks;
+axes.rinc = (axes.rmax - axes.rmin)/rticks;
 tick  = (axes.rmin + axes.rinc):axes.rinc:axes.rmax;
 
 if ~isfield(option, 'tickRMS')
