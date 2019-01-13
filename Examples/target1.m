a279b9493eea182442627be1388465e599ac579c
Peter Rochford <prochford@thesymplectic.com>
2019-01-13 18:08:34 -0500

    Corrected point of contact information and affiliation

--- Examples/target1.m
+++ Examples/target1.m
@@ -49,9 +49,9 @@
 % }
 
 % Author: Peter A. Rochford
-%         CSS-Dynamac (Contractor)
-%         NOAA/NOS/NCCOS/CCMA/COAST
-%         peter.rochford@noaa.gov
+%         Symplectic, LLC
+%         www.thesymplectic.com
+%         prochford@thesymplectic.com
 
 % Close any previously open graphics windows
 close all;
