9873ca2928ac02e9803894ecac96a8d503596fe2
Peter Rochford <prochford@thesymplectic.com>
2019-08-19 20:42:18 -0400

    Moved legend commands into add_legend function and added case of option.markerlabel as a map container. Also fixed bug with marker transparency for R2018a when creating the add_legend function.

--- plot_pattern_diagram_markers.m
+++ plot_pattern_diagram_markers.m
@@ -100,48 +100,17 @@ if strcmp(option.markerLegend,'on')
     end
     
     % Add legend
-    maxLabelLength = max(cellfun('length', markerLabel));
-    if isempty(hp)
+    if length(markerLabel) == 0
         warning('No markers within axis limit ranges.');
     else
-        if ~isempty(option.markerLabel)
-            if is_octave()
-                hLegend=legend(hp,markerLabel,'Location','northeast');
-            elseif length(markerLabel) <= 6 && maxLabelLength <= 6
-                % Put legend outside diagram
-                hLegend=legend(hp,markerLabel, ...
-                    'Location','northeastoutside','AutoUpdate','off', ...
-                    'FontSize',fontSize);
-
-                % The legend function clears marker customizations such as
-                % transparency, so restore transparency by re-updating
-                % hMarker.FaceColorData
-                legendMarkers(hp,hLegend,lMarker,alpha)
-            else
-                % Put legend to right of the plot in multiple columns as needed
-
-                nmarkers = length(markerLabel);
-                ncol = ceil(nmarkers/15);
-
-                % Plot legend of multi-column markers
-                % Note the NumColumns is only available after Matlab R2017b
-                hLegend=legend(hp,markerLabel, ...
-                    'Location','northeastoutside','AutoUpdate','off', ...
-                    'FontSize',fontSize,'NumColumns',ncol);
-
-                % The legend function clears marker customizations such as
-                % transparency, so restore transparency by re-updating
-                % hMarker.FaceColorData
-                legendMarkers(hp,hLegend,lMarker,alpha)
-            end
-        end
+        add_legend(markerLabel,option,lMarker,markerSize,fontSize,hp);
     end
     
     ht = cell(1); % create an empty cell to return
 else
     % Check if marker labels provided
     if isfield(option,'markerLabel')
-        markerLabel = 1;
+        markerLabel = option.markerLabel;
         if isempty(option.markerLabel)
             error('No marker labels provided.');
         end
@@ -173,28 +142,37 @@ else
             hm = setMarkerColor(h,option.markerColor,alpha); % Apply transparency to marker
             hp = [hp; h];
             
-            if markerLabel
+            if isa(markerLabel,'cell')
                 % Label marker
                 xtextpos = double(X(i));
                 ytextpos = double(Y(i));
-                switch labelType
-                    case 0
-                        % String array
-                        htext = text(xtextpos,ytextpos, ...
-                            option.markerLabel(i,:), ...
-                            'Color',option.markerLabelColor);
-                    case 1
-                        % Cell array
-                        htext = text(xtextpos,ytextpos, ...
-                            option.markerLabel(i), ...
-                            'Color',option.markerLabelColor);
-                    otherwise
-                        error(['Invalid label type: ' labelType]);
-                end
+                
+                htext = text(xtextpos,ytextpos, ...
+                    option.markerLabel(i), ...
+                    'Color',option.markerLabelColor);                        
+%                 switch labelType
+%                     case 0
+%                         % String array
+%                         htext = text(xtextpos,ytextpos, ...
+%                             option.markerLabel(i,:), ...
+%                             'Color',option.markerLabelColor);
+%                     case 1
+%                         % Cell array
+%                         htext = text(xtextpos,ytextpos, ...
+%                             option.markerLabel(i), ...
+%                             'Color',option.markerLabelColor);
+%                     otherwise
+%                         error(['Invalid label type: ' labelType]);
+%                 end
                 ht = [ht; htext];
             end
             hold on;
-        end
+        end % if limit test
+    end % markers loop
+    
+    % Add legend if labels provided as map
+    if isMap(markerLabel)
+        add_legend(markerLabel,option,[],markerSize,fontSize,hp);
     end
     if length(ht) > 0
         set(ht,'verticalalignment','bottom','horizontalalignment','right', ...
@@ -214,59 +192,14 @@ hold on;
 
 end %function plot_pattern_diagram_markers
 
-function legendMarkers(handle,hLegend,lMarker,alpha)
-%legendMarkers Apply color & transparency to legend markers
+function answer = isMap(A)
+%isMap Checks if object A is a Map container.
 %
-%   The legend function clears marker customizations such as
-%   transparency, so restore transparency by re-updating the
-%   markers. Also apply transparency to the symbols appearing
-%   in the legend.
-%
-%   LEGENDMARKERS(HANDLE,HLEGEND,LMARKER,ALPHA)
-%
-%   INPUTS:
-%   handle  : handle of plot
-%   hLegend : handle of legend
-%   lMarker : list of marker symbols
-%   alpha   : blending of symbol face color (0.0 transparent through 
-%             1.0 opaque). (Default : 1.0)
-%
-%   OUTPUTS:
-% 	None
+%   isMap(A)
+%   Checks if the object A is a AMp container and returns
+%     0 = false
+%     1 = true
 
-% Test for empty arrays
-if length(handle) == 0 || length(hLegend) == 0 || length(lMarker) == 0
-    error('handle is empty array');
-elseif length(hLegend) == 0
-    error('hLegend is empty array');
-elseif length(lMarker) == 0
-    error('lMarker is empty array');
-elseif length(handle) ~= length(lMarker)
-    error('handle and lMarker arrays must be same size');
-end
+answer = strcmp(class(A),'containers.Map');
 
-% Necessary to do a drawnow before operating on the legend structure
-drawnow;
-
-% Process for all markers
-for i=1:length(lMarker)
-    % Restore marker transparency
-    hm = setMarkerColor(handle(i),lMarker(i),alpha);
-end
-
-% Get legend components
-% hLegendComponents has 2 children: child 1 = LegendIcon, child 2 = Text (label)
-hLegendComponents = hLegend.EntryContainer.Children;
-for isymbol = 1:length(hLegendComponents)
-    hLegendIconComponents = hLegendComponents(isymbol).Icon.Transform.Children;
-    
-    % child 1 = Marker, child 2 = LineStrip
-    hLegendMarker = hLegendIconComponents.Children(1);
-    
-    % Set legend to same transparency as marker
-    icolor = length(hLegendComponents) + 1 - isymbol;
-    hm = setMarkerColor(hLegendMarker,lMarker(icolor),alpha); % Apply transparency to marker
-end
-
-end %function legendMarkers
-
+end %function isMap
