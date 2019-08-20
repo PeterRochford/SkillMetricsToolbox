cfe0ae98018543908a8bdeec3ccd812efe9ef5ca
Peter Rochford <prochford@thesymplectic.com>
2019-08-19 20:51:21 -0400

    Moved error checking for diagram statistics to error_check_stats function.

--- target_statistics.m
+++ target_statistics.m
@@ -42,54 +42,14 @@ function [stats] = target_statistics(predicted,reference,field,norm)
 % Validate input args
 narginchk(2,4);
 
-% Check for valid arguments
-if isstruct(predicted)
-    if ~exist('field')
-        error('FIELD argument not supplied.');
-    end
-    if ~isfield(predicted, field)
-        error(['Field is not in predicted data structure: ' field]);
-    else
-        p = getfield(predicted,field);
-    end
+if ~exist('field','var')
+    [p, r] = error_check_stats(predicted,reference);
 else
-    error('PREDICTED argument must be a data structure.');
-end
-if isstruct(reference)
-    if ~exist('field')
-        error('FIELD argument not supplied.');
-    end
-    if ~isfield(reference, field)
-        error(['Field is not in reference data structure: ' field]);
-    else
-        r = getfield(reference,field);
-    end
-else
-    error('REFERENCE argument must be a data structure.');
+    [p, r] = error_check_stats(predicted,reference,field);
 end
 
 if ~exist('norm','var')
     norm = false;
-end
-
-% Check that dimensions of predicted and reference fields match
-pdims= size(p);
-rdims= size(r);
-if length(pdims) ~= length(rdims)
-    error(['Number of predicted and reference field dimensions do not' ...
-        ' match.\n' ...
-        'length(predicted)= ' num2str(length(size(p))) ...
-        ', length(reference)= ' num2str(length(size(r))) ...
-        ],class(pdims));
-end
-for i=1:length(pdims)
-    if pdims(i) ~= rdims(i)
-        error(['Predicted and reference field dimensions do not' ...
-            ' match.\n' ...
-            'size(predicted)= ' num2str(size(p)) ...
-            ', size(reference)= ' num2str(size(r)) ...
-            ],class(pdims));
-    end
 end
 
 % Calculate bias (B)
